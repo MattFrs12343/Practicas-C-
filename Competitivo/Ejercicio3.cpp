288efb9f0614c6214ecad27a2298d12734d34017
Matias <francosalvatierram@gmail.com>
2025-07-17 11:18:57 -0400

    Ejercicio 3 Competitivo

--- Competitivo/Ejercicio3.cpp
+++ Competitivo/Ejercicio3.cpp
@@ -18,11 +18,47 @@ int main() {
 
     while(t--) 
     {
-        int n, j, k;
+        int n, j, k, i=0;
         cin >> n >> j >> k;
-        vector<int> fuerzas(n);
+        vector<int> v = fuerzas(n);
+
+        for (int i=0; i<v.size();i++)
+        {
+            if (i != j && v[i] == v[j]) //Indices diferentes pero fuerzas iguales
+            {
+                v.erase(v.begin()+i);
+                if(i<j)j--;
+            }
+            else if (i != j && v[i] < v[j])
+            {
+                v.erase(v.begin()+i);
+                if(i<j)j--;
+            }
+            else if (i != j && v[i] > v[j])
+            {
+                v.erase(v.begin()+j);
+                j=-1;
+            }
+        }
+        if (v.size() <= k)
+        {
+            cout<< "YES\n";
+        }
+        else{
+            cout<< "NO\n";
+        } 
         
     }
 
     return 0;
 }
+/*
+    
+5 2 3
+3 2 4 4 1
+5 4 1
+5 3 4 5 2
+6 1 1
+1 2 3 4 5 6
+
+*/
