ddc79ffbc0a9a292cf10f718f88e0b1c8f10d68a
Matias Franco <francosalvatierram@gmail.com>
2025-07-21 12:51:25 -0400

    Ejercicio5

---
+++ Competitivo/Ejercicio5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+bool hay3(int n) {
+    while (n > 0) {
+        if (n % 10 == 3) return true;
+        n /= 10;
+    }
+    return false;
+}
+int main()
+{
+    int t, contador=0;
+    cin>>t ;
+    for (int i = 1; i <= t; i++)
+    {
+        if (!hay3(i))
+        {
+            contador++;
+        }
+    }
+    cout<< contador;
+    return 0;
+}
