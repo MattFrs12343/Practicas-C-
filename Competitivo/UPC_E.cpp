81d2ad8130ca6f2a59430ba8e5b22235899f6a07
Matias Franco <francosalvatierram@gmail.com>
2025-07-30 17:37:32 -0400

    Ejercicio UPC

--- Competitivo/UPC_E.cpp
+++ Competitivo/UPC_E.cpp
@@ -1,7 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool capicua(string x) {
+bool palindrome(string x){
+    int n = x.length();
+    for(int i = 0; i < n; i++){
+        for(int j = n - 1 - i; j >= 0; j--){
+            if(i >= j) break; 
+            if(x[i] != x[j]) return false;
+            break; 
+        } 
+    }
+    return true;
+}
+int main() {
+    string a;
+    cin >> a;
+    cout << (palindrome(a) ? "true" : "false") << endl;
+    return 0;
+}
+
+/*bool palindrome(string x) {
     int n = x.length();
     for (int i = 0; i < n / 2; i++) {
         if (x[i] != x[n - 1 - i]) {
@@ -10,13 +28,4 @@ bool capicua(string x) {
     }
     return true;
 }
-int main() {
-    string a;
-    cin >> a;
-    cout << (capicua(a) ? "true" : "false") << endl;
-    return 0;
-}
-//abccba//
-//012345//
-//6/2=3==>Iteracion//
-//x[a] == x[a]
+    */
